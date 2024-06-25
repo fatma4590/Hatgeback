e10bc5eba8dd0002cfb62eeb0beeab47a4718819
fatma <fatma.mahmud4590@gmail.com>
2024-06-26 00:07:12 +0300

    fatma

--- lib/screens/ReservationScreen.dart
+++ lib/screens/ReservationScreen.dart
@@ -230,10 +230,12 @@ class _ReservationScreenState extends State<ReservationScreen> {
   String? _paymentMethod;
   double _fee = 0.0;
   final FirebaseAuth _auth = FirebaseAuth.instance;
+  final _firestore = FirebaseFirestore.instance;
+
   final _cardNumberController = TextEditingController();
   final _cardholderNameController = TextEditingController();
-  final _expiryDateControlleryear = TextEditingController();
-  final _expiryDateControllermonth = TextEditingController();
+  final _expiryDateControllerYear = TextEditingController();
+  final _expiryDateControllerMonth = TextEditingController();
   final _cvvController = TextEditingController();
 
   @override
@@ -294,8 +296,8 @@ class _ReservationScreenState extends State<ReservationScreen> {
                 ElevatedButton(
                   onPressed: () {
                     if (_formKey.currentState!.validate()) {
-                      _showConfirmationDialog();
                       _calculateFee();
+                      _showConfirmationDialog();
                     }
                   },
                   child: Padding(
@@ -440,7 +442,7 @@ class _ReservationScreenState extends State<ReservationScreen> {
             TextButton(
               onPressed: () {
                 Navigator.of(context).pop();
-                showvisacard();
+                _showCardDetailsDialog();
               },
               child: Text('Proceed'),
             ),
@@ -450,133 +452,187 @@ class _ReservationScreenState extends State<ReservationScreen> {
     );
   }
 
-  void showvisacard() {
+  void _showCardDetailsDialog() {
     showDialog(
-        context: context,
-        builder: (context) {
-          return AlertDialog(
-            title: Text("Card Details"),
-            content: Column(
-              children: [
-                _buildCardField(
-                  controller: _cardNumberController,
-                  labelText: 'Card Number',
-                  hintText: '1234 5678 9012 3456',
-                  keyboardType: TextInputType.number,
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter card number';
-                    }
-                    if (value.length != 16) {
-                      return 'Card number must be 16 digits';
-                    }
-                    return null;
-                  },
-                ),
-                SizedBox(height: 16),
-                _buildCardField(
-                  controller: _cardholderNameController,
-                  labelText: 'Cardholder Name',
-                  hintText: 'John Doe',
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter cardholder name';
-                    }
-                    return null;
-                  },
-                ),
-                SizedBox(height: 16),
-                Row(
-                  children: [
-                    Expanded(
-                      child: _buildCardField(
-                        controller: _expiryDateControllermonth,
-                        labelText: 'MM',
-                        hintText: 'MM',
-                        keyboardType: TextInputType.number,
-                        validator: (value) {
-                          if (value == null || value.isEmpty) {
-                            return 'Please enter expiration date month';
-                          }
-                          if (int.parse(value) < 1 || int.parse(value) > 12) {
-                            return 'Please enter a valid month';
-                          }
-                          return null;
-                        },
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                      child: Text(
-                        '/',
-                        style: TextStyle(
-                            fontSize: 24, fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                    Expanded(
-                      child: _buildCardField(
-                        controller: _expiryDateControlleryear,
-                        labelText: 'YY',
-                        hintText: 'YY',
-                        keyboardType: TextInputType.number,
-                        validator: (value) {
-                          if (value == null || value.isEmpty) {
-                            return 'Please enter expiration date year';
-                          }
-                          if (int.parse(value) < DateTime.now().year % 100) {
-                            return 'Please enter a valid year';
-                          }
-                          return null;
-                        },
-                      ),
-                    ),
-                  ],
-                ),
-                SizedBox(height: 16),
-                _buildCardField(
-                  controller: _cvvController,
-                  labelText: 'CVV',
-                  hintText: '123',
-                  keyboardType: TextInputType.number,
-                  obscureText: true,
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter CVV';
-                    }
-                    if (value.length != 3) {
-                      return 'CVV must be 3 digits';
-                    }
-                    return null;
-                  },
-                ),
-                SizedBox(height: 24),
-                ElevatedButton(
-                  onPressed: () {
-                    if (_formKey.currentState!.validate()) {
-                      _submitReservation();
-                      Navigator.of(context).pushReplacement(
-                        MaterialPageRoute(builder: (context) => homepage()),
-                      );
-                    }
-                  },
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                        vertical: 15.0, horizontal: 20.0),
-                    child: Text('Save Card', style: TextStyle(fontSize: 16)),
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text("Card Details"),
+          content: Form(
+            key: _formKey, // Ensure card details form is validated
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  _buildCardField(
+                    controller: _cardNumberController,
+                    labelText: 'Card Number',
+                    hintText: '1234 5678 9012 3456',
+                    keyboardType: TextInputType.number,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter card number';
+                      }
+                      if (value.length != 16) {
+                        return 'Card number must be 16 digits';
+                      }
+                      return null;
+                    },
                   ),
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.green, // Button background color
-                    foregroundColor: Colors.white, // Button text color
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
-                    elevation: 5.0,
+                  SizedBox(height: 16),
+                  _buildCardField(
+                    controller: _cardholderNameController,
+                    labelText: 'Cardholder Name',
+                    hintText: 'John Doe',
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter cardholder name';
+                      }
+                      return null;
+                    },
                   ),
-                ),
-              ],
+                  SizedBox(height: 16),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: _buildCardField(
+                          controller: _expiryDateControllerMonth,
+                          labelText: 'MM',
+                          hintText: 'MM',
+                          keyboardType: TextInputType.number,
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter expiration date month';
+                            }
+                            if (int.parse(value) < 1 || int.parse(value) > 12) {
+                              return 'Please enter a valid month';
+                            }
+                            return null;
+                          },
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                        child: Text(
+                          '/',
+                          style: TextStyle(
+                              fontSize: 24, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      Expanded(
+                        child: _buildCardField(
+                          controller: _expiryDateControllerYear,
+                          labelText: 'YY',
+                          hintText: 'YY',
+                          keyboardType: TextInputType.number,
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter expiration date year';
+                            }
+                            if (int.parse(value) <
+                                DateTime.now().year % 100) {
+                              return 'Please enter a valid year';
+                            }
+                            return null;
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 16),
+                  _buildCardField(
+                    controller: _cvvController,
+                    labelText: 'CVV',
+                    hintText: '123',
+                    keyboardType: TextInputType.number,
+                    obscureText: true,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter CVV';
+                      }
+                      if (value.length != 3) {
+                        return 'CVV must be 3 digits';
+                      }
+                      return null;
+                    },
+                  ),
+                  SizedBox(height: 24),
+                  ElevatedButton(
+                    onPressed: () async {
+                      if (_formKey.currentState!.validate()) {
+                        final isCardValid = await _checkCardInFirestore(
+                          cardNumber: _cardNumberController.text,
+                          cardholderName: _cardholderNameController.text,
+                          expiryMonth: _expiryDateControllerMonth.text,
+                          expiryYear: _expiryDateControllerYear.text,
+                          cvv: _cvvController.text,
+                        );
+
+                        if (isCardValid) {
+                          _submitReservation();
+                          Navigator.of(context).pushReplacement(
+                            MaterialPageRoute(
+                                builder: (context) => homepage()),
+                          );
+                        } else {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(
+                              content: Text('Card not found. Please try again.'),
+                              backgroundColor: Colors.red,
+                            ),
+                          );
+                        }
+                      }
+                    },
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 15.0, horizontal: 20.0),
+                      child: Text('Save Card', style: TextStyle(fontSize: 16)),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.green, // Button background color
+                      foregroundColor: Colors.white, // Button text color
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      elevation: 5.0,
+                    ),
+                  ),
+                ],
+              ),
             ),
-          );
-        });
+          ),
+        );
+      },
+    );
+  }
+
+  Future<bool> _checkCardInFirestore({
+    required String cardNumber,
+    required String cardholderName,
+    required String expiryMonth,
+    required String expiryYear,
+    required String cvv,
+  }) async {
+    final user = FirebaseAuth.instance.currentUser;
+    if (user == null) {
+      // User not authenticated
+      return false;
+    }
+    final userId = user.uid;
+
+    // Query the user's cards subcollection for matching card details
+    final cardSnapshot = await _firestore
+        .collection('users')
+        .doc(userId)
+        .collection('cards')
+        .where('cardNumber', isEqualTo: int.parse(cardNumber))
+        .where('cardholderName', isEqualTo: cardholderName)
+        .where('expiryDatemonth', isEqualTo: expiryMonth)
+        .where('expiryDateyear', isEqualTo: expiryYear)
+        .where('cvv', isEqualTo: int.parse(cvv))
+        .get();
+
+    return cardSnapshot.docs.isNotEmpty;
   }
 
   void _submitReservation() async {
@@ -604,7 +660,7 @@ class _ReservationScreenState extends State<ReservationScreen> {
       'parkingName': widget.parkingArea['Name'],
     };
 
-    await FirebaseFirestore.instance
+    await _firestore
         .collection('Reservations')
         .doc(widget.parkingArea['Name'])
         .set(reservation);
@@ -618,33 +674,34 @@ class _ReservationScreenState extends State<ReservationScreen> {
 
     Navigator.of(context).pop();
   }
+
+  Widget _buildCardField({
+    required TextEditingController controller,
+    required String labelText,
+    required String hintText,
+    TextInputType keyboardType = TextInputType.text,
+    bool obscureText = false,
+    required FormFieldValidator<String> validator,
+  }) {
+    return TextFormField(
+      controller: controller,
+      decoration: InputDecoration(
+        labelText: labelText,
+        hintText: hintText,
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        filled: true,
+        fillColor: Colors.white,
+        contentPadding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 20.0),
+      ),
+      keyboardType: keyboardType,
+      obscureText: obscureText,
+      validator: validator,
+    );
+  }
 }
 
-Widget _buildCardField({
-  required TextEditingController controller,
-  required String labelText,
-  required String hintText,
-  TextInputType keyboardType = TextInputType.text,
-  bool obscureText = false,
-  required FormFieldValidator<String> validator,
-}) {
-  return TextFormField(
-    controller: controller,
-    decoration: InputDecoration(
-      labelText: labelText,
-      hintText: hintText,
-      border: OutlineInputBorder(
-        borderRadius: BorderRadius.circular(10.0),
-      ),
-      filled: true,
-      fillColor: Colors.white,
-      contentPadding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 20.0),
-    ),
-    keyboardType: keyboardType,
-    obscureText: obscureText,
-    validator: validator,
-  );
-}
 
 
 
